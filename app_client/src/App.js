39dcd6bd9b6d5d66f5554590775c00ac4d2d1a39
unknown <you@example.com>
2018-02-25 18:38:44 -0800

    few components

---
+++ app_client/src/App.js
@@ -0,0 +1,4 @@
+import React from "react";
+import Game from "./components/Game";
+const App = () => <Game />;
+export default App;
---
+++ app_client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+    <footer className= "footer">
+        <div classname="bottom">
+        Clicky Game!
+        </div>
+    </footer>
+
+);
+
+export default Footer;
